210c498ca64c92a2dfd578342ca1e12aab2b40a0
supryantowp <supryantowp21@gmail.com>
2020-10-17 23:24:04 +0700

    fix connection

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'pgsl'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
