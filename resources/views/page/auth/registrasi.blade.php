991bddaffd7f176902ad48531195fb4ac4f6dc22
Dendy <54370384+dendyjuliano@users.noreply.github.com>
2020-10-19 00:18:18 +0700

    Update registrasi.blade.php

--- resources/views/page/auth/registrasi.blade.php
+++ resources/views/page/auth/registrasi.blade.php
@@ -7,7 +7,7 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="{{ asset('backend/auth/css/style_registrasi.css') }}">
+	<link rel="stylesheet" type="text/css" href="backend/auth/css/style_registrasi.css">
 
     <title>Registrasi</title>
   </head>
