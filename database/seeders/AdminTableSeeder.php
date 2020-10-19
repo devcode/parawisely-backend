ba0156ee4e64a99b32495e8af2417b7f9e0f9824
supryantowp <supryantowp21@gmail.com>
2020-10-19 11:07:52 +0700

    test

--- database/seeders/AdminTableSeeder.php
+++ database/seeders/AdminTableSeeder.php
@@ -14,6 +14,7 @@ class AdminTableSeeder extends Seeder
      */
     public function run()
     {
+        #eho
         Employee::create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
